6c4c01fa5cacf8ecd324ef2fee2682e655cda330
PedroProcter <OProcter89@gmail.com>
2022-11-29 17:19:02 -0400

    refacotr: Implemente Correct Sugar strips Testing

--- tests/test_sugar_strips.py
+++ tests/test_sugar_strips.py
@@ -1,9 +1,14 @@
-
+from sugar_strips import SugarStrips
 
 
 
 def test_return_not_sugar_strips_left():
-    assert False
+    sugar_strips = SugarStrips(5)
+
+    assert not sugar_strips.has_sugar_strip()
 
 def test_return_less_sugar_trips_left():
-    assert False
+    sugar_strips = SugarStrips(5)
+    sugar_strips.get_sugar_strip()
+    
+    assert (sugar_strips.length() == 4)
