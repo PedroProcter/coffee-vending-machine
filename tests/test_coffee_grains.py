dabd451fa136f520bdfb61a7aa8e6c565565d965
PedroProcter <OProcter89@gmail.com>
2022-11-29 17:36:49 -0400

    refactor: Implement Correct Test for Coffe Machine

--- tests/test_coffee_grains.py
+++ tests/test_coffee_grains.py
@@ -1,9 +1,14 @@
-
+from coffee_machine import CoffeeMachine
 
 
 
 def test_return_not_coffee_grains_left():
-    assert False
+    coffee_machine = CoffeeMachine(0)
+
+    assert not coffee_machine.has_coffe_grains()
 
 def test_return_less_coffee_grains_left():
-    assert False
+    coffee_machine = CoffeeMachine(5)
+    coffee_machine.get_coffee()
+
+    assert (coffee_machine.length() == 4)
