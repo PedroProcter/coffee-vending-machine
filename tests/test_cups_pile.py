f6b63332a377b82814b058fb6b2aed49803e7d30
PedroProcter <OProcter89@gmail.com>
2022-11-29 17:12:53 -0400

    refactor: Refactor Cups Tests

--- tests/test_cups_pile.py
+++ tests/test_cups_pile.py
@@ -5,26 +5,25 @@ from cups_pile import PileOfCups, Cup
 def test_return_not_cups_left():
     pile_of_cups = PileOfCups(0)
 
-    assert pile_of_cups.has_cups()
+    assert not pile_of_cups.has_cups()
 
 def test_return_small_cup():
     pile_of_small_cups = PileOfCups(5)
 
-    assert pile_of_small_cups.get_cup()
+    assert (type(pile_of_small_cups.get_cup()) == Cup)
 
 def test_return_medium_cup():
     pile_of_medium_cups = PileOfCups(5)
-    cup = Cup()
     
-    assert (pile_of_medium_cups.get_cup() == cup)
+    assert (type(pile_of_medium_cups.get_cup()) == Cup)
 
 def test_return_big_cup():
     pile_of_big_cups = PileOfCups(5)
-    
-    assert pile_of_big_cups.get_cup()
+
+    assert (type(pile_of_big_cups.get_cup()) == Cup)
 
 def test_return_less_cups_left():
-    pile_of_cups = PileOfCups(0)
-    pile_of_cups.has_cups()
+    pile_of_cups = PileOfCups(5)
+    pile_of_cups.get_cup()
 
-    assert pile_of_cups.length()
+    assert (pile_of_cups.length() == 4)
